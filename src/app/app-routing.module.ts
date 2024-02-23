3d2a9ac77ddc327e69e4c4d239ec8563973b4118
Chunpong Chan <122968364+65011212003@users.noreply.github.com>
2024-02-23 14:41:53 +0700

    123

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ProfileconfigComponent } from './pages/profileconfig/profileconfig.component';
+import { VotesComponent } from './pages/votes/votes.component';
+import { UpdateComponent } from './pages/update/update.component';
+import { ChangepasswordComponent } from './pages/changepassword/changepassword.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'profig', component: ProfileconfigComponent }
+  { path: 'profig', component: ProfileconfigComponent },
+  { path: 'votes', component: VotesComponent },
+  { path: 'update', component: UpdateComponent },
+  { path: 'changepassword', component: ChangepasswordComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './pages/main/main.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ProfileconfigComponent } from './pages/profileconfig/profileconfig.component';
+import { VotesComponent } from './pages/votes/votes.component';
+import { UpdateComponent } from './pages/update/update.component';
+import { ChangepasswordComponent } from './pages/changepassword/changepassword.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     LoginComponent,
-    ProfileconfigComponent
+    ProfileconfigComponent,
+    VotesComponent,
+    UpdateComponent,
+    ChangepasswordComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
